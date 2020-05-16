25d6020d4e4151f60320bcce87e94d3c182ef760
Michael Causey <michaelhcausey@gmail.com>
2020-05-16 22:48:29 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,19 @@
 # Write your methods here
+
+def loop_message_five_times(message)
+  count = 0
+  while count < 5 do
+    puts message
+    count += 1
+  end
+end
+
+def loop_message_n_times
+  
+end
+
+def output_array
+  
+end
+
+def return_string_array
